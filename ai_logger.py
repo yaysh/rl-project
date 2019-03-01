451685085e9a22e1dcbaadeedd0eba01f3499847
Simon Karlsson <simon.karlsson.93@hotmail.com>
2019-03-01 22:08:21 +0100

    prettier logging

--- ai_logger.py
+++ ai_logger.py
@@ -1,8 +1,16 @@
 from IPython import display 
 from collections import deque
 
-log_queue = deque(maxlen=10)
-def log(text):
-    log_queue.append(text)
-    display.clear_output(wait=True)
-    print("\n".join(log_queue), flush=True)
+class Logger:
+    
+    def __init__(self, n_rows, header=None):
+        self.log_queue = deque(maxlen=n_rows)
+        self.header = header
+    
+    
+    def log(self, text):
+        self.log_queue.append(text)
+        display.clear_output(wait=True)
+        if self.header != None:
+            print(self.header)
+        print("\n".join(self.log_queue), flush=True)

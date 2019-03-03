d9504ad64fc06f75e1f2483975a7b88d5b8434c7
Simon Karlsson <simon.karlsson.4@volvocars.com>
2019-03-03 15:28:40 +0100

    Add trajectory trail

--- ai_display.py
+++ ai_display.py
@@ -1,21 +1,20 @@
 import matplotlib.pyplot as plt
 from IPython import display
+import numpy as np
 
 def show_state(observation, env_id, step=0, info=""):
     plt.figure(3)
     plt.clf()
     
     if len(observation)==4:
-        fig, axes = plt.subplots(nrows=1, ncols=4, figsize=(10,4))
-        for i in range(4):
-            axes[i].imshow(observation[i], cmap='gray')
-            axes[i].set_axis_off()
-            axes[i].set_aspect('auto')
-            fig.tight_layout()
-    else:
-        plt.imshow(observation, cmap='gray')
-        plt.axis('off')
-
+        frames = list()
+        for i, val in enumerate([0.1, 0.2, 0.3, 0.4]):
+            frames.append(np.multiply(observation[i], val))
+        print(np.array(frames).shape)
+        observation = np.sum(frames, axis=0)
+        
+    plt.imshow(observation, cmap='gray')
+    plt.axis('off')
     plt.title("%s | Step: %d %s" % (env_id, step, info))
     display.clear_output(wait=True)
     display.display(plt.gcf())
